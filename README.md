871c7d2eb8809f4513a65678e1099c17c3632456
Murli0399 <murlikhaire28@gmail.com>
2023-07-13 21:02:09 +0530

    13/07/2023

---
+++ README.md
@@ -0,0 +1,9 @@
+## for creating a new react app 
+```
+npx create-react-app my-react-app
+```
+
+## for starting a react app
+```
+npm start
+```
---
+++ hello/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./Components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="hello">Hello World!</p>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
